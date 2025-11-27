e645af38dbbda4356e928c7b5b7e9b87a9ecca89
Maksim <kodermax@gmail.com>
2025-11-27 14:21:00 +0300

    fix

--- packages/api/src/routers/company/get.ts
+++ packages/api/src/routers/company/get.ts
@@ -2,8 +2,10 @@ import { eq } from "@selectio/db";
 import { company } from "@selectio/db/schema";
 import { protectedProcedure } from "../../trpc";
 
-export const get = protectedProcedure.query(({ ctx }) => {
-  return ctx.db.query.company.findFirst({
+export const get = protectedProcedure.query(async ({ ctx }) => {
+  const result = await ctx.db.query.company.findFirst({
     where: eq(company.userId, ctx.session.user.id),
   });
+
+  return result ?? null;
 });
