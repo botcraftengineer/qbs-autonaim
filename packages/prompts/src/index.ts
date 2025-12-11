de1302dbb0d3caff5fa61ea7f789f99543d0a1a9
Maksim <kodermax@gmail.com>
2025-12-11 08:29:27 +0300

    up

--- packages/prompts/src/index.ts
+++ packages/prompts/src/index.ts
@@ -1,6 +1,7 @@
 export * from "./candidate-prompts";
 export * from "./interview-prompts";
 export * from "./screening-prompts";
+export * from "./telegram-recruiter-prompts";
 export * from "./templates";
 export * from "./utils/name-extractor";
 export * from "./vacancy-prompts";
