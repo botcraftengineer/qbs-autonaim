6cf4c3689bb4e7970c93f396d2e523aa5cb8c5ad
Maksim <kodermax@gmail.com>
2025-12-16 16:29:54 +0300

    up

---
+++ apps/web/next.config.ts
@@ -0,0 +1,28 @@
+import { createJiti } from "jiti";
+import { NextConfig } from "next";
+
+export default async function createNextConfig(): Promise<NextConfig> {
+  const jiti = createJiti(import.meta.url);
+
+  // Import env files to validate at build time. Use jiti so we can load .ts files in here.
+  await jiti.import("./src/env");
+
+  /** @type {import("next").NextConfig} */
+  const config: NextConfig = {
+    /** Enables hot reloading for local packages without a build step */
+    output: "standalone",
+
+    transpilePackages: [
+      "@qbs-autonaim/auth",
+      "@qbs-autonaim/db",
+      "@qbs-autonaim/lib",
+      "@qbs-autonaim/ui",
+      "@qbs-autonaim/validators",
+    ],
+
+    /** We already do linting and typechecking as separate tasks in CI */
+    typescript: { ignoreBuildErrors: true },
+  };
+
+  return config;
+}
