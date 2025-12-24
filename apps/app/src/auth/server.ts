7a68225772bbc0c91417afc71ed5eee620a48842
Maksim <kodermax@gmail.com>
2025-12-24 16:56:40 +0300

    up

--- apps/app/src/auth/server.ts
+++ apps/app/src/auth/server.ts
@@ -8,16 +8,11 @@ import { nextCookies } from "better-auth/next-js";
 import { headers } from "next/headers";
 import { cache } from "react";
 
-const baseUrl =
-  env.VERCEL_ENV === "production"
-    ? `https://${env.VERCEL_PROJECT_PRODUCTION_URL}`
-    : env.VERCEL_ENV === "preview"
-      ? `https://${env.VERCEL_URL}`
-      : "http://localhost:3000";
+const baseUrl = env.APP_URL ?? "http://localhost:3000";
 
 export const auth = initAuth({
   baseUrl,
-  productionUrl: `https://${env.VERCEL_PROJECT_PRODUCTION_URL ?? "turbo.t3.gg"}`,
+  productionUrl: env.APP_URL ?? "http://localhost:3000",
   secret: env.AUTH_SECRET,
   googleClientId: env.AUTH_GOOGLE_ID,
   googleClientSecret: env.AUTH_GOOGLE_SECRET,
