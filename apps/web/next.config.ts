336bbe6368afd6990ae53ba8c39eda6e9b03c1e3
Maksim <kodermax@gmail.com>
2025-12-24 15:53:47 +0300

    up

---
+++ apps/web/next.config.ts
@@ -0,0 +1,20 @@
+import { createJiti } from "jiti";
+import { NextConfig } from "next";
+
+export default async function createNextConfig(): Promise<NextConfig> {
+  const jiti = createJiti(import.meta.url);
+
+  // Import env files to validate at build time. Use jiti so we can load .ts files in here.
+  await jiti.import("./src/env");
+
+  /** @type {import("next").NextConfig} */
+  const config: NextConfig = {
+    /** Enables hot reloading for local packages without a build step */
+    output: "standalone",
+
+    /** We already do linting and typechecking as separate tasks in CI */
+    typescript: { ignoreBuildErrors: true },
+  };
+
+  return config;
+}
