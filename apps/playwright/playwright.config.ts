c8a950584d892f86445ecedae0153dd9f9e2f65b
Maksim <kodermax@gmail.com>
2025-12-26 19:21:42 +0300

    up

--- apps/playwright/playwright.config.ts
+++ apps/playwright/playwright.config.ts
@@ -4,7 +4,7 @@ const baseURL = process.env.BASE_URL || "http://localhost:3000";
 
 export default defineConfig({
   testDir: "./tests",
-  fullyParallel: false,
+  fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: 1, // Один воркер = последовательное выполнение
