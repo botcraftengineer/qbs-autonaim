31259a957fbc054ed1307a8f1cfff0eea380eae1
Maksim <kodermax@gmail.com>
2026-01-12 13:54:21 +0300

    up

--- packages/db/src/schema/vacancy/relations.ts
+++ packages/db/src/schema/vacancy/relations.ts
@@ -1,14 +1,16 @@
 import { relations } from "drizzle-orm";
 import { user } from "../auth";
+import { response } from "../response/response";
 import { workspace } from "../workspace/workspace";
 import { freelanceImportHistory } from "./freelance-import-history";
 import { vacancy } from "./vacancy";
 
-export const vacancyRelations = relations(vacancy, ({ one }) => ({
+export const vacancyRelations = relations(vacancy, ({ one, many }) => ({
   workspace: one(workspace, {
     fields: [vacancy.workspaceId],
     references: [workspace.id],
   }),
+  responses: many(response),
 }));
 
 export const freelanceImportHistoryRelations = relations(
